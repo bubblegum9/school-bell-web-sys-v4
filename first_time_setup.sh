f4237e89e25c9e1c68d597d684d7ab7aa13b6230
Alex <66681971+bubblegum9@users.noreply.github.com>
2023-06-21 19:08:07 +0200

    Update first_time_setup.sh

--- first_time_setup.sh
+++ first_time_setup.sh
@@ -11,32 +11,16 @@ echo "INSTALLING PYTHON3 PIP, SMBUS (FOR LCD), VENV"
 yes | sudo apt install python3-pip
 yes | sudo apt install python3-smbus
 yes | sudo apt install python3-venv
-echo "INSTALLING PYTHON LIB'S"
-source llamabell/bin/activate
-pip3 install flask
-pip3 install RPi.GPIO
-pip3 install python-dotenv
-pip3 install tinydb
-pip3 install gpiozero
-pip3 install smbus
-pip3 install gunicorn
-deactivate
-pip3 install flask
-pip3 install RPi.GPIO
-pip3 install python-dotenv
-pip3 install tinydb
-pip3 install gpiozero
-pip3 install smbus
-pip3 install gunicorn
-sudo pip3 install flask
-sudo pip3 install RPi.GPIO
-sudo pip3 install python-dotenv
-sudo pip3 install tinydb
-sudo pip3 install gpiozero
-sudo pip3 install smbus
-sudo pip3 install gunicorn
 
-#python3 -m venv llamabell
+pip3 install -I flask RPi.GPIO python-dotenv tinydb gpiozero smbus gunicorn
+sudo pip3 install flask RPi.GPIO python-dotenv tinydb gpiozero smbus gunicorn
+
+echo "INSTALLING PYTHON LIB'S"
+source /home/lex/school-bell-web-sys-v4/venv-coffee/bin/activate
+pip3 install -I flask RPi.GPIO python-dotenv tinydb gpiozero smbus gunicorn
+deactivate
+
+#python3 -m venv venv-tofu
 echo "run: source llamabell/bin/activate"
 echo "to turn off type deactivate"
 
@@ -46,7 +30,7 @@ echo "to turn off type deactivate"
 #sudo cp coffeebell.conf /etc/apache2/sites-available/
 # create apache2 symbolic link
 # (a2ensite is a part of apache2)
-sudo a2ensite coffeebell.conf
-sudo systemctl enable coffeebell.service
-sudo systemctl start coffeebell.service
-sudo systemctl reload apache2
+#sudo a2ensite coffeebell.conf
+#sudo systemctl enable coffeebell.service
+#sudo systemctl start coffeebell.service
+#sudo systemctl reload apache2
