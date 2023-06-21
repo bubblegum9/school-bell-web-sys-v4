0c8774093a3f9794a178f689b89981aae96c0354
crackMaker <aleksa.andric064@gmail.com>
2023-06-21 20:24:15 +0200

    .

--- manual_start.sh
+++
@@ -1,1 +0,0 @@
-/home/zvono/school-bell-web-sys-v4/venv-coffee/bin/gunicorn --config gunicorn.py wsgi:app
--- restart_all.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo systemctl restart coffeebell.service
-sudo systemctl restart apache2.service
